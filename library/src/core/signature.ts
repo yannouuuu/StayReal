a049a80d6f00ebcb8254bdab3eb5152f7d9c869b
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-29 03:40:17 +0100

    feat: add signature generator

---
+++ library/src/core/signature.ts
@@ -0,0 +1,16 @@
+import { utf8ToBytes, concatBytes } from '@noble/hashes/utils';
+import { sha256 } from '@noble/hashes/sha2';
+import { hmac } from '@noble/hashes/hmac';
+import { base64 } from '@scure/base';
+
+import { BEREAL_HMAC_KEY, BEREAL_TIMEZONE } from "~/constants";
+
+export const createBeRealSignature = (deviceID: string): string => {
+  const timestamp = `${Math.floor(Date.now() / 1000)}`;
+  
+  const data = utf8ToBytes(`${deviceID}${BEREAL_TIMEZONE}${timestamp}`);
+  const hash = hmac(sha256, BEREAL_HMAC_KEY, base64.encode(data));
+  const bytes = concatBytes(utf8ToBytes(`1:${timestamp}:`), hash);
+
+  return base64.encode(bytes);
+};
