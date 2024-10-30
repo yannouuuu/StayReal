2a910794b364b00f10a0d57df3c7562a850cf46c
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-30 02:33:13 +0100

    chore: add refresh api export (and remove old ios exports)

--- library/src/index.ts
+++ library/src/index.ts
@@ -2,16 +2,12 @@ export {
   // to replicate the challenges client-side
   BEREAL_ARKOSE_PUBLIC_KEY,
   BEREAL_RECAPTCHA_SITE_KEY,
-
-  // just for the sake of it
-  BEREAL_IOS_BUNDLE_ID,
-  BEREAL_IOS_VERSION,
-  BEREAL_IOS_BUILD,
 } from "~/constants";
 
 // so we can authenticate
 export * from "~/api/auth/vonage/request";
 export * from "~/api/auth/vonage/verify";
+export * from "~/api/auth/refresh";
 export * from "~/api/moments/last";
 export * from "~/api/person/me";
 
