fc56478909ef98a9cdef8734b358935833c86afc
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-31 17:16:43 +0100

    feat(api): react with realmoji to a specific post

---
+++ src/api/requests/content/realmojis.ts
@@ -0,0 +1,22 @@
+import auth from "../../../stores/auth";
+import { BEREAL_DEFAULT_HEADERS } from "../../constants";
+import { fetch } from "@tauri-apps/plugin-http";
+
+// TODO: typings
+
+export const content_realmojis_put = async (postId: string, postUserId: string, emoji: string): Promise<any> => {
+  /// example of the body sent :
+  /// { "emoji": "😍" }
+
+  const response = await fetch(`https://mobile.bereal.com/api/content/realmojis?postId=${postId}&postUserId=${postUserId}`, {
+    method: "PUT",
+    headers: {
+      ...BEREAL_DEFAULT_HEADERS(auth.store.deviceID),
+      authorization: `Bearer ${auth.store.accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ emoji }),
+  });
+
+  return response.json();
+}
