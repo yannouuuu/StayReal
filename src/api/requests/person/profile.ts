14bc0bf3927b04518a7c755c4695ae9838b28b84
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-10-31 17:17:58 +0100

    feat(api): profile of a given user

---
+++ src/api/requests/person/profile.ts
@@ -0,0 +1,44 @@
+import { BEREAL_DEFAULT_HEADERS } from "../../constants";
+import { fetch } from "@tauri-apps/plugin-http";
+import { ApiMedia } from "../../types/media";
+import auth from "../../../stores/auth";
+
+export interface PersonProfile {
+  id: string
+  username: string
+  fullname: string
+  profilePicture: ApiMedia | null
+  relationship: {
+    status: "pending" | "accepted"
+    commonFriends: {
+      sample: Array<unknown> // TODO
+      total: number
+    }
+    friendedAt: string
+  }
+  createdAt: string
+  isRealPeople: boolean
+  beRealOfTheDay: { postExists: boolean }
+  userFreshness: "returning" | "new"
+  streakLength: number
+  type: "USER"
+  links: Array<unknown>
+  isPrivate: boolean
+}
+
+export const person_profile = async (id: string): Promise<PersonProfile> => {
+  const response = await fetch(`https://mobile.bereal.com/api/person/profiles/${id}?withPost=true`, {
+    headers: {
+      ...BEREAL_DEFAULT_HEADERS(auth.store.deviceID),
+      authorization: `Bearer ${auth.store.accessToken}`
+    }
+  });
+
+  // if token expired, refresh it and retry
+  if (response.status === 401) {
+    await auth.refresh();
+    return person_profile(id);
+  }
+
+  return response.json();
+};
