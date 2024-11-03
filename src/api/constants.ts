4be4beb2abe4b6e05e63a6f45792e2a9309cff1a
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-03 14:29:12 +0100

    chore(constants): use hex instead of base16
    it's more lore accurate when you're dumping the key from loggers

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
-import { base16 } from '@scure/base';
+import { hex } from '@scure/base';
 import { createBeRealSignature } from "./core/signature";
 
 export const BEREAL_ANDROID_BUNDLE_ID = "com.bereal.ft";
@@ -11,7 +11,7 @@ export const BEREAL_TIMEZONE = "Europe/Paris";
 export const BEREAL_CLIENT_SECRET_KEY = "F5A71DA-32C7-425C-A3E3-375B4DACA406"
 export const BEREAL_ARKOSE_PUBLIC_KEY = "CCB0863E-D45D-42E9-A6C8-9E8544E8B17E";
 export const BEREAL_RECAPTCHA_SITE_KEY = "6LfqjDgoAAAAAPy3wiCP92R3nDyNgDDIsjZACoVT";
-export const BEREAL_HMAC_KEY = base16.decode('3536303337663461663232666236393630663363643031346532656337316233')
+export const BEREAL_HMAC_KEY = hex.decode('3536303337663461663232666236393630663363643031346532656337316233')
 
 export const BEREAL_DEFAULT_HEADERS = (deviceID: string) => ({
   "bereal-platform": "android",
