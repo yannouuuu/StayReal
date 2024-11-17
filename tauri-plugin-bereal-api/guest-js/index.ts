56ae0ccfe2cddf38afa00c6a1b882babd86ce442
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-17 02:47:08 +0100

    feat(plugin/guest): add wrapper functions to invoke rust functions

--- tauri-plugin-bereal-api/guest-js/index.ts
+++ tauri-plugin-bereal-api/guest-js/index.ts
@@ -1,10 +1,25 @@
-import { invoke } from '@tauri-apps/api/core'
+import { invoke } from '@tauri-apps/api/core';
 
-/**
- * Initialize the BeReal Moment API notification for the given region.
- */
-export async function initBeRealMomentForRegion(region: string): Promise<void> {
-  await invoke<{region?: string}>('plugin:mobile-bereal-api|ping', {
-    payload: { region }
+export interface AuthDetails {
+  deviceId: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export async function setAuthDetails(payload: AuthDetails): Promise<void> {
+  return invoke('plugin:bereal-api|set_auth_details', {
+    payload
   });
+}
+
+export async function getAuthDetails(): Promise<AuthDetails> {
+  return invoke('plugin:bereal-api|get_auth_details');
+}
+
+export async function clearAuthDetails(): Promise<void> {
+  return invoke('plugin:bereal-api|clear_auth_details');
+}
+
+export async function refreshToken(): Promise<void> {
+  return invoke('plugin:bereal-api|refresh_token');
 }
