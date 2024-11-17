959efa9b50136cec5ec594e9969de93367098649
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-17 02:45:09 +0100

    chore(plugin): add http and work runtime implementations

--- tauri-plugin-bereal-api/android/build.gradle.kts
+++ tauri-plugin-bereal-api/android/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.vexcited.stayreal.bereal.api"
+    namespace = "com.vexcited.stayreal.api"
     compileSdk = 34
 
     defaultConfig {
@@ -34,9 +34,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.0")
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
