4e681e8d49a02ee341e249701ebc9e26eb735f6d
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-18 01:51:53 +0100

    feat(auth): retry refresh token a few times before giving up
    sometimes the API can return `400` when hitting rate limit, so we retry a few times to make sure we're unable to refresh

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,6 +1,7 @@
 import { createRoot, onMount } from "solid-js";
 import { createStore } from "solid-js/store";
 import { refreshToken, getAuthDetails, type AuthDetails, setAuthDetails, clearAuthDetails } from "tauri-plugin-bereal-api";
+import { wait } from "../utils/wait";
 
 export default createRoot(() => {
   const [store, setStore] = createStore<{ loading: boolean } & AuthDetails>({
@@ -10,10 +11,26 @@ export default createRoot(() => {
     refreshToken: "",
   });
 
-  const refresh = async () => {
-    await refreshToken(); // refresh it natively
-    const mutation = await getAuthDetails();
-    setStore({ ...mutation });
+  const refresh = async (retry = 0): Promise<void> => {
+    try {
+      await refreshToken(); // refresh it natively
+      const mutation = await getAuthDetails();
+      setStore({ ...mutation });
+    }
+    catch (error) {
+      // we might be limited by the API sometimes, we receive a 400 error
+      // so we retry a few times before giving up
+      if (retry < 2) {
+        console.info(`auth.refresh[retry=${retry}]: failed, retrying in 1s`);
+
+        await wait(1000);
+        return refresh(retry + 1);
+      }
+      else {
+        console.error("auth.refresh:", error);
+        // TODO: show a toast telling the user to restart the app in a few minutes or when back online
+      }
+    }
   };
 
   const save = async (mutation: AuthDetails) => {
