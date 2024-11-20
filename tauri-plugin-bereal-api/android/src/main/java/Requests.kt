f573d60b4a8ab8ab38a7ca82b8a9671c150e7814
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-21 00:09:53 +0100

    feat(android): implement fetch last moment request

--- tauri-plugin-bereal-api/android/src/main/java/Requests.kt
+++ tauri-plugin-bereal-api/android/src/main/java/Requests.kt
@@ -4,19 +4,19 @@ import android.content.Context
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.Headers
-import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.RequestBody
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import java.util.Locale
 import java.util.TimeZone
 import org.json.JSONObject
 import java.io.IOException
+import com.android.build.gradle.internal.cxx.json.jsonStringOf
 
 class Requests (private val context: Context) {
   val authentication = Authentication(context)
+  val preferences = Preferences(context)
 
   private val appAndroidBundleId = "com.bereal.ft"
   private val appAndroidVersion = "3.10.1"
@@ -41,20 +41,20 @@ class Requests (private val context: Context) {
   }
 
   suspend fun refreshToken () {
-    val details = authentication.get()
+    val auth = authentication.get()
 
     val json = JSONObject().apply {
       put("client_id", "android")
       put("grant_type", "refresh_token")
       put("client_secret", clientSecretKey)
-      put("refresh_token", details.refreshToken)
+      put("refresh_token", auth.refreshToken)
     }
 
     val client = OkHttpClient()
 
     val request = Request.Builder()
       .url("https://auth.bereal.com/token")
-      .headers(defaultHeaders(details.deviceId!!))
+      .headers(defaultHeaders(auth.deviceId!!))
       .post(json.toString().toRequestBody("application/json".toMediaType()))
       .build()
 
@@ -73,10 +73,33 @@ class Requests (private val context: Context) {
 
     authentication.set(
       AuthenticationDetails(
-        deviceId = details.deviceId,
+        deviceId = auth.deviceId,
         accessToken = jsonBody.getString("access_token"),
         refreshToken = jsonBody.getString("refresh_token")
       )
+    )
+  }
+
+  suspend fun fetchLastMoment (): Moment {
+    val region = preferences.getRegion()
+    val client = OkHttpClient()
+
+    val request = Request.Builder()
+      .url("https://mobile.bereal.com/api/bereal/moments/last/$region")
+      .build()
+
+    val response = withContext(Dispatchers.IO) {
+      client.newCall(request).execute()
+    }
+
+    val body = response.body!!.toString()
+    val jsonBody = JSONObject(body)
+
+    return Moment(
+      momentId = jsonBody.getString("id"),
+      startDate = jsonBody.getString("start_date"),
+      endDate = jsonBody.getString("end_date"),
+      region = jsonBody.getString("region")
     )
   }
 }
