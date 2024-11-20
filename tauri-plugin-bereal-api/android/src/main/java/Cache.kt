0520e810bf707db88c918a6db01acb3592e09952
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-21 00:09:15 +0100

    feat(android): implement cache for moment ID

---
+++ tauri-plugin-bereal-api/android/src/main/java/Cache.kt
@@ -0,0 +1,28 @@
+package com.vexcited.stayreal.api
+
+import android.content.Context
+
+class Cache (private val context: Context) {
+  private val preferencesName = "Cache"
+
+  fun setLastMomentId (id: String) {
+    val sharedPreferences = context.getSharedPreferences(
+      preferencesName,
+      Context.MODE_PRIVATE
+    )
+
+    with(sharedPreferences.edit()) {
+      putString("lastMomentId", id)
+      apply()
+    }
+  }
+
+  fun getLastMomentId (): String? {
+    val sharedPreferences = context.getSharedPreferences(
+      preferencesName,
+      Context.MODE_PRIVATE
+    )
+
+    return sharedPreferences.getString("lastMomentId", null)
+  }
+}
