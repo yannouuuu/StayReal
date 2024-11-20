b57a8c68b347f2d77560670d2ab096e083dad069
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-11-21 00:10:20 +0100

    feat(android): Moment data class

---
+++ tauri-plugin-bereal-api/android/src/main/java/Moment.kt
@@ -0,0 +1,10 @@
+package com.vexcited.stayreal.api
+
+data class Moment (
+  val momentId: String,
+  // ex: "2024-11-20T16:50:27.930Z"
+  val startDate: String,
+  // ex: "2024-11-20T16:52:27.930Z"
+  val endDate: String,
+  val region: String
+)
