3c892d7d13265d17f863c2840b49648b43d4bd85
Yann Renard <yannrenard1025@gmail.com>
2025-01-02 01:16:42 +0100

    refactor(feed, friends): improve UI
    - Reorganized imports for better readability in FeedView and FriendsView components.
    - Enhanced layout and styling in both views, including header adjustments and improved fallback messages.
    - Streamlined the rendering logic for user and friend profiles, ensuring consistent presentation.
    - Updated class names for better responsiveness and maintainability.

--- src/views/feed.tsx
+++ src/views/feed.tsx
@@ -1,14 +1,14 @@
 import { createSignal, For, onMount, Show, type Component } from "solid-js";
-import FeedFriendsOverview from "../components/feed/friends/overview";
-import MdiPeople from '~icons/mdi/people';
-import MdiRefresh from '~icons/mdi/refresh'
 import me from "~/stores/me";
+import MdiPeople from "~icons/mdi/people";
+import MdiRefresh from "~icons/mdi/refresh";
+import FeedFriendsOverview from "../components/feed/friends/overview";
 
+import { fetchLastMoment, type Moment } from "@stayreal/api";
+import feed from "~/stores/feed";
 import FeedUserOverview from "../components/feed/user/overview";
 import PullableScreen from "../components/pullable-screen";
-import { fetchLastMoment, type Moment } from "@stayreal/api";
 import { tryToStartNotificationService } from "../utils/notification-service";
-import feed from "~/stores/feed";
 
 const FeedView: Component = () => {
   const [moment, setMoment] = createSignal<Moment>();
@@ -20,30 +20,28 @@ const FeedView: Component = () => {
       setIsRefreshing(true);
 
       await me.refetch();
-      await Promise.all([
-        feed.refetch(),
-        fetchLastMoment().then(setMoment)
-      ])
-    }
-    finally {
+      await Promise.all([feed.refetch(), fetchLastMoment().then(setMoment)]);
+    } finally {
       setIsRefreshing(false);
     }
   };
 
-  onMount(() => Promise.all([
-    tryToStartNotificationService(),
-    handleRefresh()
-  ]));
+  onMount(() =>
+    Promise.all([tryToStartNotificationService(), handleRefresh()])
+  );
 
   return (
     <div>
-      <header class="z-20 fixed top-0 inset-x-0 bg-gradient-to-b from-black to-transparent pt-[env(safe-area-inset-top)]">
+      <header class="z-20 fixed top-0 inset-x-0 bg-gradient-to-b from-black to-transparent pt-[env(safe-area-inset-top)] max-h-[72px] h-full">
         <nav class="flex items-center justify-between px-4 pb-2 pt-4">
           <a href="/friends" aria-label="Relationships">
             <MdiPeople class="text-xl" />
           </a>
 
-          <p class="absolute inset-x-0 w-fit mx-auto text-2xl text-center text-white font-700" role="banner">
+          <p
+            class="absolute inset-x-0 w-fit mx-auto text-2xl text-center text-white font-700"
+            role="banner"
+          >
             StayReal.
           </p>
 
@@ -54,22 +52,24 @@ const FeedView: Component = () => {
               disabled={isRefreshing()}
               title="Refresh feed & last moment"
             >
-              <MdiRefresh class="text-white text-2xl rounded-full p-1"
+              <MdiRefresh
+                class="text-white text-2xl rounded-full p-1"
                 classList={{
-                  "animate-spin text-white/50 bg-white/10": isRefreshing()
+                  "animate-spin text-white/50 bg-white/10": isRefreshing(),
                 }}
               />
             </button>
 
             <a href="/profile" aria-label="My profile">
-              <Show when={me.get()?.profilePicture}
+              <Show
+                when={me.get()?.profilePicture}
                 fallback={
                   <div>
                     <p>{me.get()?.username[0] || "?"}</p>
                   </div>
                 }
               >
-                {profilePicture => (
+                {(profilePicture) => (
                   <img
                     class="w-8 h-8 rounded-full"
                     src={profilePicture().url}
@@ -81,45 +81,70 @@ const FeedView: Component = () => {
           </div>
         </nav>
       </header>
-      
+
       <div class="py-16 mt-[env(safe-area-inset-top)]">
         <PullableScreen
           onRefresh={handleRefresh}
           shouldPullToRefresh={!isScrolling()}
         >
           <main>
-            <Show when={feed.get()} fallback={
-              <p class="text-center text-white/50">
-                finding your feed...
-              </p>
-            }>
-              {feed => (
+            <Show
+              when={feed.get()}
+              fallback={
+                <p class="text-center text-white/50">finding your feed...</p>
+              }
+            >
+              {(feed) => (
                 <>
-                  <Show when={feed().userPosts} fallback={
-                    <div class="text-center flex flex-col gap-1 px-4 mx-4 bg-white/10 py-4 rounded-2xl">
-                      <p class="mb-4">
-                        You haven't posted any BeReal today !
-                      </p>
+                  <Show
+                    when={feed().userPosts}
+                    fallback={
+                      <div class="text-center flex flex-col gap-1 px-4 mx-4 bg-white/10 py-4 rounded-2xl">
+                        <p class="mb-4">
+                          You haven't posted any BeReal today !
+                        </p>
 
-                      <a href="/upload" class="block text-center py-3 font-600 bg-white text-black rounded-2xl">
-                        StayReal by posting a BeReal.
-                      </a>
+                        <a
+                          href="/upload"
+                          class="block text-center py-3 font-600 bg-white text-black rounded-2xl"
+                        >
+                          StayReal by posting a BeReal.
+                        </a>
 
-                      <Show when={moment()}>
-                        {moment => (
-                          <p class="text-white/50 mt-1 text-xs">
-                            Last moment was at {new Date(moment().startDate).toLocaleTimeString()}
-                          </p>
-                        )}
-                      </Show>
-                    </div>
-                  }>
-                    {overview => <FeedUserOverview overview={overview()} setScrolling={setIsScrolling} />}
+                        <Show when={moment()}>
+                          {(moment) => (
+                            <p class="text-white/50 mt-1 text-xs">
+                              Last moment was at{" "}
+                              {new Date(
+                                moment().startDate
+                              ).toLocaleTimeString()}
+                            </p>
+                          )}
+                        </Show>
+                      </div>
+                    }
+                  >
+                    {(overview) => (
+                      <FeedUserOverview
+                        overview={overview()}
+                        setScrolling={setIsScrolling}
+                      />
+                    )}
                   </Show>
 
                   <div class="flex flex-col gap-6 mt-8">
-                    <For each={[...feed().friendsPosts].sort((a, b) => new Date(b.posts[b.posts.length - 1].postedAt).getTime() - new Date(a.posts[a.posts.length - 1].postedAt).getTime())}>
-                      {overview => (
+                    <For
+                      each={[...feed().friendsPosts].sort(
+                        (a, b) =>
+                          new Date(
+                            b.posts[b.posts.length - 1].postedAt
+                          ).getTime() -
+                          new Date(
+                            a.posts[a.posts.length - 1].postedAt
+                          ).getTime()
+                      )}
+                    >
+                      {(overview) => (
                         <FeedFriendsOverview
                           overview={overview}
                           setScrolling={setIsScrolling}
@@ -140,7 +165,7 @@ const FeedView: Component = () => {
         </PullableScreen>
       </div>
     </div>
-  )
+  );
 };
 
 export default FeedView;
--- src/views/friends.tsx
+++ src/views/friends.tsx
@@ -6,63 +6,74 @@ const FriendsView: Component = () => {
   const [friends] = createResource(relationships_friends);
 
   return (
-    <div class="pt-[env(safe-area-inset-top)] pb-20vh mb-[env(safe-area-inset-bottom)]">
-      <header class="mb-12">
-        <nav class="flex items-center justify-end px-4 pb-2 pt-4">
-          <a href="/feed">
+    <div>
+      <header class="z-20 fixed top-0 inset-x-0 bg-gradient-to-b from-black to-transparent pt-[env(safe-area-inset-top)] max-h-[72px] h-full">
+        <nav class="flex items-center justify-between px-4 pb-2 pt-4">
+          <div class="w-8" />
+
+          <p
+            class="absolute inset-x-0 w-fit mx-auto text-2xl text-center text-white font-700"
+            role="banner"
+          >
+            StayReal.
+          </p>
+
+          <a href="/feed" aria-label="Return Feed">
             <MdiChevronRight class="text-xl" />
           </a>
         </nav>
       </header>
 
-      <Show
-        when={friends()}
-        fallback={
-          <p class="text-center text-white/50">gathering your friends...</p>
-        }
-      >
-        {(friends) => (
-          <main class="px-4">
-            <p class="text-sm text-white/60 uppercase font-600 mb-4">
-              My Friends ({friends()?.total})
-            </p>
+      <div class="py-16 mt-[env(safe-area-inset-top)]">
+        <Show
+          when={friends()}
+          fallback={
+            <p class="text-center text-white/50">gathering your friends...</p>
+          }
+        >
+          {(friends) => (
+            <main class="px-4">
+              <p class="text-sm text-white/60 uppercase font-600 mb-4">
+                My Friends ({friends()?.total})
+              </p>
 
-            <div class="flex flex-col gap-4">
-              <For each={friends().data}>
-                {(friend) => (
-                  <div class="flex items-center gap-4">
-                    <div class="relative">
-                      <Show
-                        when={friend.profilePicture}
-                        fallback={
-                          <div class="rounded-full h-15 w-15 bg-white/20 flex items-center justify-center">
-                            <span class="text-[24px] font-500 uppercase leading-[0] translate-y-[2px]">
-                              {friend.username[0]}
-                            </span>
-                          </div>
-                        }
-                      >
-                        {(profilePicture) => (
-                          <img
-                            class="rounded-full h-15 w-15"
-                            src={profilePicture().url}
-                            alt={friend.username}
-                          />
-                        )}
-                      </Show>
-                    </div>
+              <div class="flex flex-col gap-4">
+                <For each={friends().data}>
+                  {(friend) => (
+                    <div class="flex items-center gap-4">
+                      <div class="relative">
+                        <Show
+                          when={friend.profilePicture}
+                          fallback={
+                            <div class="rounded-full h-15 w-15 bg-white/20 flex items-center justify-center">
+                              <span class="text-[24px] font-500 uppercase leading-[0] translate-y-[2px]">
+                                {friend.username[0]}
+                              </span>
+                            </div>
+                          }
+                        >
+                          {(profilePicture) => (
+                            <img
+                              class="rounded-full h-15 w-15"
+                              src={profilePicture().url}
+                              alt={friend.username}
+                            />
+                          )}
+                        </Show>
+                      </div>
 
-                    <div class="flex flex-col">
-                      <p class="font-500">{friend.fullname}</p>
-                      <p class="text-sm text-white/60">@{friend.username}</p>
+                      <div class="flex flex-col">
+                        <p class="font-500">{friend.fullname}</p>
+                        <p class="text-sm text-white/60">@{friend.username}</p>
+                      </div>
                     </div>
-                  </div>
-                )}
-              </For>
-            </div>
-          </main>
-        )}
-      </Show>
+                  )}
+                </For>
+              </div>
+            </main>
+          )}
+        </Show>
+      </div>
     </div>
   );
 };
