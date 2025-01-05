8d2d386c7cc0dbfacd191364c17fde33dbc6faaf
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-05 06:46:55 +0100

    refactor(sig): make timestamp a property with default value

--- src/api/core/signature.ts
+++ src/api/core/signature.ts
@@ -1,13 +1,10 @@
+import { BEREAL_HMAC_KEY, BEREAL_TIMEZONE } from "~/api/constants";
 import { utf8ToBytes, concatBytes } from '@noble/hashes/utils';
 import { sha256 } from '@noble/hashes/sha2';
 import { hmac } from '@noble/hashes/hmac';
 import { base64 } from '@scure/base';
 
-import { BEREAL_HMAC_KEY, BEREAL_TIMEZONE } from "../constants";
-
-export const createBeRealSignature = (deviceID: string): string => {
-  const timestamp = `${Math.floor(Date.now() / 1000)}`;
-  
+export const createBeRealSignature = (deviceID: string, timestamp = Math.floor(Date.now() / 1000)): string => {
   const data = utf8ToBytes(`${deviceID}${BEREAL_TIMEZONE}${timestamp}`);
   const hash = hmac(sha256, BEREAL_HMAC_KEY, base64.encode(data));
   const prefix = utf8ToBytes(`1:${timestamp}:`);
