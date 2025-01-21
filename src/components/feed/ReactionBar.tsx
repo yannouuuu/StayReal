93b894834e0ed63cb4f9be55053b2e69fcdef7a9
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-21 23:38:13 +0100

    feat(feed): add reaction bar on posts

---
+++ src/components/feed/ReactionBar.tsx
@@ -0,0 +1,75 @@
+import { createMemo, createSignal, For, Show, type Component } from "solid-js";
+import { content_realmojis_delete, content_realmojis_put } from "~/api/requests/content/realmojis";
+import { type PostsOverview } from "~/api/requests/feeds/friends";
+import me from "~/stores/me";
+
+const ReactionBar: Component<{
+  post: PostsOverview["posts"][number]
+  postUserId: string
+  onReact: () => void
+}> = (props) => {
+  const [loading, setLoading] = createSignal(false);
+
+  const currentReaction = createMemo(() => {
+    const reaction = props.post.realMojis.find(r => r.user.id === me.get().id);
+    return reaction ?? null;
+  });
+
+  const react = async (emoji: string): Promise<void> => {
+    try {
+      setLoading(true);
+
+      const reaction = currentReaction();
+      if (reaction?.emoji === emoji) {
+        await content_realmojis_delete(props.post.id, props.postUserId, reaction.id);
+      }
+      else {
+        await content_realmojis_put(props.post.id, props.postUserId, emoji);
+      }
+    }
+    catch {
+      // TODO
+      console.error("failed to react to the post.");
+    }
+    finally {
+      setLoading(false);
+      props.onReact();
+    }
+  };
+
+  return (
+    <div class="flex items-center justify-center gap-2 animate-duration-450"
+      classList={{
+        "animate-pulse": loading(),
+      }}
+    >
+      <For each={me.get().realmojis}>
+        {(realmoji) => (
+          <button type="button" class="relative" onClick={() => react(realmoji.emoji)}>
+            <p class="absolute bottom-0 right-0 text-sm z-40"
+              classList={{
+                "opacity-70": currentReaction() !== null && currentReaction()?.emoji !== realmoji.emoji,
+              }}
+            >
+              {realmoji.emoji}
+            </p>
+
+            <Show when={currentReaction() !== null && currentReaction()?.emoji !== realmoji.emoji}>
+              <div class="z-35 inset-0 absolute bg-black/40 rounded-full" />
+            </Show>
+
+            <img
+              class="shrink-0 rounded-full border-2 border-black w-13 h-13 bg-black z-30"
+              src={realmoji.media.url}
+              classList={{
+                "outline outline-1 outline-white": currentReaction()?.emoji === realmoji.emoji,
+              }}
+            />
+          </button>
+        )}
+      </For>
+    </div>
+  )
+};
+
+export default ReactionBar;
--- src/components/feed/friends/post.tsx
+++ src/components/feed/friends/post.tsx
@@ -4,10 +4,12 @@ import { FeedPost } from "~/api/requests/feeds/friends";
 import PostRealMojis from "~/components/feed/realmojis";
 import feed from "~/stores/feed";
 import me from "~/stores/me";
+import SolarSmileCircleBold from '~icons/solar/smile-circle-bold'
+import ReactionBar from "../ReactionBar";
 
 const FeedFriendsPost: Component<{
   post: FeedPost
-  
+
   /**
    * User ID of the post owner.
    */
@@ -16,17 +18,18 @@ const FeedFriendsPost: Component<{
   const [isReversed, setIsReversed] = createSignal(false);
   const [videoRef, setVideoRef] = createSignal<HTMLVideoElement>();
   const [isFocusing, setIsFocusing] = createSignal(false);
+  const [isReacting, setIsReacting] = createSignal(false);
 
   const primaryURL = () => isReversed() ? props.post.secondary.url : props.post.primary.url;
   const secondaryURL = () => isReversed() ? props.post.primary.url : props.post.secondary.url;
 
   let timer: ReturnType<typeof setTimeout> | undefined;
-  
+
   // On mobile, when having pointer down and scrolling
   // doesn't trigger pointerup, but pointercancel instead.
   // That's why we need to handle both.
   const upEvents = ["pointerup", "pointercancel", "pointermove"];
-  
+
   const handleVideoUnfocus = () => {
     if (timer) clearTimeout(timer);
     setIsFocusing(false);
@@ -52,6 +55,13 @@ const FeedFriendsPost: Component<{
     // We ignore right-clicking.
     if (event.pointerType === "mouse" && event.button !== 0) return;
     if (timer) clearTimeout(timer);
+
+    // If we're currently reacting, clicking on the focused image
+    // should remove the reaction bar.
+    if (isReacting()) {
+      setIsReacting(false);
+      return;
+    }
 
     if (videoRef()) {
       upEvents.forEach(name => document.addEventListener(name, handleVideoUnfocus));
@@ -93,7 +103,7 @@ const FeedFriendsPost: Component<{
   const isCurrentlyReposted = () => (feed.get()?.userPosts?.posts ?? []).some(post =>
     post.origin === "repost" && post.parentPostId === props.post.id
   );
-  
+
   return (
     <div class="z-20 relative mx-auto w-fit">
       <img
@@ -134,21 +144,52 @@ const FeedFriendsPost: Component<{
       </Show>
 
       {/* dimmed background overlay */}
-      <div class="z-25 absolute inset-x-0 h-50px bottom-0 bg-gradient-to-t from-black/50 to-transparent" />
-      
+      <Show when={isReacting()}>
+        <div class="z-25 absolute inset-x-0 h-50px bottom-0 bg-gradient-to-t from-black/50 to-transparent" />
+      </Show>
+
       {/* small realmojis in the bottom left */}
       <div class="w-fit">
         <div class="absolute z-30 bottom-4 left-4 transition-opacity"
           classList={{
-            "opacity-0 pointer-events-none": isFocusing()
+            "opacity-0 pointer-events-none": isFocusing() || isReacting()
           }}
         >
           <PostRealMojis post={props.post} size={2} shouldReverseZIndex />
         </div>
       </div>
 
+      <div class="w-fit">
+        <div class="absolute z-30 bottom-2 right-4 transition-opacity"
+          classList={{
+            "opacity-0 pointer-events-none": isFocusing() || isReacting()
+          }}
+        >
+          <button type="button"
+            onClick={() => setIsReacting(true)}
+          >
+            <SolarSmileCircleBold class="text-white text-3xl text-shadow-xl" />
+          </button>
+        </div>
+      </div>
+
+      {/* Reaction using realmojis feature. */}
+      <div class="absolute z-30 left-4 right-4 transition-all"
+        classList={{
+          "opacity-0 pointer-events-none -bottom-6": !isReacting(),
+          "bottom-4": isReacting()
+        }}
+      >
+        <ReactionBar post={props.post} postUserId={props.postUserId}
+          onReact={() => {
+            setIsReacting(false);
+            feed.refetch();
+          }}
+        />
+      </div>
+
       {/*
-        * Repost feature : 
+        * Repost feature :
         * We should show it only when the current user is tagged in the post
         * and the post is not already reposted by the current user.
         */}
