c6fed8dd2f9401e591a1f0ae03f9e926a13ee8d7
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-21 23:37:11 +0100

    feat(api): add type for realmojis put and add realmojis delete on posts

--- src/api/requests/content/realmojis.ts
+++ src/api/requests/content/realmojis.ts
@@ -1,13 +1,24 @@
+import { ApiMedia } from "~/api/types/media";
 import auth from "../../../stores/auth";
 import { BEREAL_DEFAULT_HEADERS } from "../../constants";
 import { fetch } from "@tauri-apps/plugin-http";
 
-// TODO: typings
+export type RealmojisPut = {
+  id: string
+  user: {
+    id: string
+    username: string
+    profilePicture: ApiMedia
+    type: "USER"
+  }
+  emoji: string
+  isInstant: boolean
+  media: ApiMedia
+  /** @example "2025-01-21T21:30:56.125Z" */
+  postedAt: string
+}
 
-export const content_realmojis_put = async (postId: string, postUserId: string, emoji: string): Promise<any> => {
-  /// example of the body sent :
-  /// { "emoji": "😍" }
-
+export const content_realmojis_put = async (postId: string, postUserId: string, emoji: string): Promise<RealmojisPut> => {
   const response = await fetch(`https://mobile-l7.bereal.com/api/content/realmojis?postId=${postId}&postUserId=${postUserId}`, {
     method: "PUT",
     headers: {
@@ -16,6 +27,20 @@ export const content_realmojis_put = async (postId: string, postUserId: string, 
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ emoji }),
+  });
+
+  return response.json();
+}
+
+export const content_realmojis_delete = async (postId: string, postUserId: string, realmoji_id: string): Promise<void> => {
+  const response = await fetch(`https://mobile-l7.bereal.com/api/content/realmojis?postId=${postId}&postUserId=${postUserId}`, {
+    method: "DELETE",
+    headers: {
+      ...BEREAL_DEFAULT_HEADERS(auth.store.deviceId),
+      authorization: `Bearer ${auth.store.accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ realmojiIds: [realmoji_id] }),
   });
 
   return response.json();
