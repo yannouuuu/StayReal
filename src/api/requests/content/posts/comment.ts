d78a83aad87e036f8a11929d52bad73fc6ed9856
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-22 22:27:51 +0100

    feat(api): add request to post a comment under a post

---
+++ src/api/requests/content/posts/comment.ts
@@ -0,0 +1,31 @@
+import { fetch } from "@tauri-apps/plugin-http";
+import { BEREAL_DEFAULT_HEADERS } from "~/api/constants";
+import { ApiMedia } from "~/api/types/media";
+import auth from "~/stores/auth";
+
+export type CommentPost = {
+  id: string
+  user: {
+    id: string
+    username: string
+    profilePicture?: ApiMedia
+    type: "USER"
+  }
+  content: string
+  /** @example "2025-01-21T21:30:56.125Z" */
+  postedAt: string
+}
+
+export const content_posts_comment = async (postId: string, postUserId: string, content: string): Promise<CommentPost> => {
+  const response = await fetch(`https://mobile-l7.bereal.com/api/content/comments?postId=${postId}&postUserId=${postUserId}`, {
+    method: "POST",
+    headers: {
+      ...BEREAL_DEFAULT_HEADERS(auth.store.deviceId),
+      authorization: `Bearer ${auth.store.accessToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ content }),
+  });
+
+  return response.json();
+}
