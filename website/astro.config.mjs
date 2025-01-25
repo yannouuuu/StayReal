4f0d7d14180bb6ad61713d09db69f5b9f1171496
Mikkel RINGAUD <mikkel@milescode.dev>
2025-01-25 10:22:51 +0100

    chore: add vercel adapter

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,7 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import unocss from 'unocss/astro'
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [unocss({ injectReset: true })]
 });
+
