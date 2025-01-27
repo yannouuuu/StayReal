f1e4e250508010623b0f1c8abeb68b70b6c522dc
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-27 23:17:28 +0100

    feat: add a friend post in the feed for demo

--- src/api/demo/feeds/friends.ts
+++ src/api/demo/feeds/friends.ts
@@ -1,18 +1,60 @@
 import { FeedsFriends } from "~/api/requests/feeds/friends";
-import { deepMerge } from "~/utils/deep-merge";
+import { DEMO_RELATIONSHIPS_FRIENDS_LIST } from "../relationships/friends/list";
 
-let data = {
+export const DEMO_FEEDS_FRIENDS: FeedsFriends = {
   eventProtoBytes: [],
   maxPostsPerMoment: 1,
-  friendsPosts: [],
+  friendsPosts: [
+    {
+      contentMappingEnabled: false,
+      momentId: "moment-123456",
+      region: "europe-west",
+      user: {
+        countryCode: "FR",
+        id: DEMO_RELATIONSHIPS_FRIENDS_LIST.data[0].id,
+        profilePicture: null,
+        type: "USER",
+        username: DEMO_RELATIONSHIPS_FRIENDS_LIST.data[0].username
+      },
+      posts: [{
+        comments: [],
+        creationDate: new Date().toISOString(),
+        id: DEMO_RELATIONSHIPS_FRIENDS_LIST.data[0].id + "-post-123456",
+        isFirst: true,
+        isLate: false,
+        isMain: true,
+        isResurrected: false,
+        lateInSeconds: 0,
+        postedAt: new Date().toISOString(),
+        momentId: "moment-123456",
+        postType: "default",
+        primary: {
+          // Taken from Vexcited's profile.
+          url: "https://cdn-us1.bereal.network/Photos/QCP7u3Ado8Q1GIvAw4bXfNKggSA2/post/-ygM_srX5dWuiOaj.webp",
+          height: 2000,
+          width: 1500,
+          mimeType: "image/webp",
+          mediaType: "image"
+        },
+        secondary: {
+          // Taken from Vexcited's profile.
+          url: "https://cdn-us1.bereal.network/Photos/QCP7u3Ado8Q1GIvAw4bXfNKggSA2/post/UMzdNNw0A3HT8HSl.webp",
+          height: 2000,
+          width: 1500,
+          mimeType: "image/webp",
+          mediaType: "image"
+        },
+        realMojis: [],
+        retakeCounter: 0,
+        tags: [],
+        takenAt: new Date().toISOString(),
+        unblurCount: 0,
+        updatedAt: new Date().toISOString(),
+        visibility: ["friends"],
+        createdAt: new Date().toISOString()
+      }]
+    }
+  ],
   remainingPosts: 0,
   userPosts: null
-} satisfies FeedsFriends;
-
-export const DEMO_FEEDS_FRIENDS = (migration?: Partial<FeedsFriends>): FeedsFriends => {
-  if (migration) {
-    data = deepMerge(data, migration);
-  }
-
-  return data;
 };
